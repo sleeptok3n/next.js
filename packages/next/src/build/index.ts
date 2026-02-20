823d6e1d2d665c66ceb064c74d728eb73e7b202f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-20 19:07:04 +0100

    adjust NEXT_CLIENT_ASSET_SUFFIX

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -858,9 +858,10 @@ export function createStaticWorker(
             }
           : undefined),
         // worker.ts copies this value into globalThis.NEXT_CLIENT_ASSET_SUFFIX
-        __NEXT_PRERENDER_CLIENT_ASSET_SUFFIX: config.deploymentId
-          ? `?dpl=${config.deploymentId}`
-          : '',
+        __NEXT_PRERENDER_CLIENT_ASSET_SUFFIX:
+          config.experimental.immutableAssetToken || config.deploymentId
+            ? `?dpl=${config.experimental.immutableAssetToken || config.deploymentId}`
+            : '',
       },
     },
   }) as StaticWorker
@@ -2064,6 +2065,7 @@ export default async function build(
               cacheLifeProfiles: config.cacheLife,
               buildId,
               deploymentId: config.deploymentId,
+              immutableAssetToken: config.experimental.immutableAssetToken,
               sriEnabled,
               cacheMaxMemorySize: config.cacheMaxMemorySize,
             })
@@ -2290,6 +2292,8 @@ export default async function build(
                             cacheLifeProfiles: config.cacheLife,
                             buildId,
                             deploymentId: config.deploymentId,
+                            immutableAssetToken:
+                              config.experimental.immutableAssetToken,
                             sriEnabled,
                           })
                         }
